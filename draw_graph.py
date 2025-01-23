b64c2a7eaea07664f0f4da5017695ec2b24aeb65
Zandri2k <sandrasiphokaziii@gmail.com>
2025-01-23 10:20:56 +0100

    test

--- draw_graph.py
+++ draw_graph.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 df = pd.DataFrame(     {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
-print(df)
+print(df          )
 plt.plot(             df["x"], df["y"                  ])
 
 

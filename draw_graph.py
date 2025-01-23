15099a1220ec48d1060095e50fe4ea33f077a9b6
Zandri2k <sandrasiphokaziii@gmail.com>
2025-01-23 10:19:36 +0100

    testing te commit

--- draw_graph.py
+++ draw_graph.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
 
+
+
+
+
+df = pd.DataFrame(     {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
 print(df)
-plt.plot(df["x"], df["y"])
+plt.plot(             df["x"], df["y"                  ])
+
+
 
 
 plt.show()

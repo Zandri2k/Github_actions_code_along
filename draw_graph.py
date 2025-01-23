31ab3675dae78c804759ab17a4a3c5ae56065a03
Zandri2k <sandrasiphokaziii@gmail.com>
2025-01-23 10:25:17 +0100

    ny version

--- draw_graph.py
+++ draw_graph.py
@@ -1,18 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
-
-
-
-
-
-
-df = pd.DataFrame(     {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
-print(df          )
-plt.plot(             df["x"], df["y"                  ])
-
-
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
+print(df)
+plt.plot(df["x"], df["y"])
 
 
 plt.show()

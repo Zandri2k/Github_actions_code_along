ac4eb5e8d260c19249570f0d55458633117cb7bf
Zandri2k <sandrasiphokaziii@gmail.com>
2025-01-23 10:11:59 +0100

    formating

--- draw_graph.py
+++ draw_graph.py
@@ -3,18 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
 
-
-
-
-
-
-
-df = pd.DataFrame({"x:" np.array([1,2,3]), "y": nparray([2,5,8]  )}   )
 print(df)
 plt.plot(df["x"], df["y"])
-
-
 
 
 plt.show()
